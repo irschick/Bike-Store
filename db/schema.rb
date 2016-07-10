c0eb486e06a6a8ad4435d6b6bd27b479ad1a9a04
irschick <michael.irschick@gmail.com>
2016-07-09 22:51:45 -0700

    Add existing file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710045810) do
+ActiveRecord::Schema.define(version: 20160710054920) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160710045810) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
