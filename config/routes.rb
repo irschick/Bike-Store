8546bdf3c2fc4a5ed732149b632cf5921935d3a8
irschick <michael.irschick@gmail.com>
2016-08-12 11:38:49 -0700

    added new root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # root 'static_pages#landing_page'
   root 'static_pages#about'
 
