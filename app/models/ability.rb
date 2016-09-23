55d8b871170076f3b2e07a6121d08f1a0c705fb4
irschick <michael.irschick@gmail.com>
2016-09-23 14:36:50 -0700

    admin user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,11 @@ class Ability
   include CanCan::Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    can :read, Product
     can :manage, User, id: user.id
+    can [:read, :create], Comment
+    if user.admin?
+      can :manage, :all
+    end
   end
 end
